98a211c5cff7a71b11d96db61c64fa36e68f534f
misaelsarm <misaelsarmiento97@gmail.com>
2022-07-03 23:52:37 -0500

    blog ui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      "1.gravatar.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,20 +1,46 @@
-import Head from 'next/head'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
 import Layout from '../components/Layout'
+import styles from '../styles/Blog.module.scss'
+import Image from 'next/image';
 
-export default function Index({ allPosts: { edges }, preview }: any) {
-  const heroPost = edges[0]?.node
+export default function Index({ allPosts: { edges } }: any) {
   const morePosts = edges
 
   console.log(morePosts);
 
   return (
     <Layout title='Blog' >
-      <Head>
-        <title>Next.js Blog Example with {CMS_NAME}</title>
-      </Head>
-      
+
+      <div className={styles.header}>
+        <Image
+          layout='fill'
+          objectFit='cover'
+          src='/blog/blog-header.jpeg'
+          alt=''
+        />
+        <div className="wave1"></div>
+      </div>
+      <div className={styles.posts}>
+        {
+          morePosts.map((post: any) => (
+            <div key={post.node.title} className={styles.post}>
+              <div className={styles.image}>
+                <Image
+                  layout='fill'
+                  src={post.node.featuredImage.node.sourceUrl}
+                  alt=''
+                />
+              </div>
+              <h4>{post.node.title}</h4>
+              <span
+                dangerouslySetInnerHTML={{ __html: post.node.excerpt }}
+              ></span>
+              <span>{post.node.date}</span>
+
+            </div>
+          ))
+        }
+      </div>
     </Layout>
   )
 }
